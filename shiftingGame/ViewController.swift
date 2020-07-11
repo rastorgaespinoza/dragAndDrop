7a686fab751f3d06003d1db68f1d7e1f09c957f4
Rodrigo <r.astorgaespinoz@gmail.com>
2020-07-11 10:22:49 -0400

    Initial Commit

---
+++ shiftingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shiftingGame
+//
+//  Created by Rodrigo Astorga on 11-07-20.
+//  Copyright © 2020 Rodrigo Astorga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
